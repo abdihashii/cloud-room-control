137e007b9be021376df49e96bcd38ef1d9c0f212
Abdirahman <abdirahman.haji.13@gmail.com>
2016-01-13 05:17:53 -0600

    Fixed getJSON error handling by using .fail(function()) instead of $.ajax error:

--- script/main.js
+++ script/main.js
@@ -1,29 +1,3 @@
-ajaxCall(requestURLLock, locked, unlocked, isLocked);
-
-
-$("#refresh-icon").rotate({ 
-   bind: 
-     { 
-        click: function(){
-            $(this).rotate({ angle: 0, animateTo:180, duration: 350})
-        }
-     } 
-});
-
-$('.status').css('display','none');
-$('#waiting').css('display','inline');
-
-var dots = 0;
-setInterval(waiting, 800);
-function waiting() {
-  if (dots < 3) {
-    $('#dots').append('.');
-    dots++;
-  } else {
-    $('#dots').html('');
-    dots = 0;
-  }
-}
 
 // Initializing vars for status
 var isLocked = "isLocked";
@@ -32,8 +6,49 @@ var deviceID = "26001c000247343337373738";
 var accessToken = "aa0358abcb3e3ba75775ba6c65a02f9099145d44";
 var baseURL = "https://api.particle.io/v1/devices/";
 
-var requestURLLock = baseURL + deviceID + "/" + isLocked;
-var requestURLLight = baseURL + deviceID + "/" + isOn;
+requestURLLock = baseURL + deviceID + "/" + isLocked + "/?access_token=" + accessToken;
+requestURLLight = baseURL + deviceID + "/" + isOn + "/?access_token=" + accessToken;
+
+var flagS = false;
+var flagF = false;
+function call(URL, status1, status2) {
+  $.getJSON(URL, function(json) {
+    $('.status').css('display','none');
+    $('#connected').css('display','inline');
+    if (!flagS) {
+      console.log('Connected to Photon!');
+      flagS = true;
+    }
+    flagF = false;
+    if (json.result === true){ // locked
+      status1();
+    }
+    else if (json.result === false){ // unlocked
+      status2();
+    }
+  }).fail(function(){
+    $('.status').css('display','none');
+    $('#disconnected').css('display','inline');
+    if (!flagF) {
+      console.log('Disconnected from Photon!');
+      flagF = true;
+    }
+    flagS = false;
+  });
+}
+
+/***** LOCKSTATUS *****/
+call(requestURLLock, locked, unlocked);
+/***** LIGHTSTATUS *****/
+call(requestURLLight, lightOn, lightOff);
+
+/***** REFRESH status *****/
+function refresh() {
+  // LOCK STATUS
+  call(requestURLLock, locked, unlocked);
+  // LIGHTSTATUS
+  call(requestURLLight, lightOn, lightOff);  
+}
 
 
 
@@ -63,7 +78,6 @@ function lock() {
     // unlocked();
     doorLock("UNLOCK");
   }
-  ajaxCall(requestURLLight, lightOn, lightOff, isOn);
   // refresh();
 }
 function locked() {
@@ -87,7 +101,6 @@ function light() {
     // lightOff();
     lightSwitch("OFF");
   }
-  ajaxCall(requestURLLight, lightOn, lightOff, isOn);
   // refresh();
 }
 function lightOn() {
@@ -105,11 +118,11 @@ function lightOff() {
 
 /***** home AUTOMATION *****/
 function home(value) {
-  if (value === true) {
+  if (value == true) {
     doorLock("LOCK");
     lightSwitch("OFF");
   }
-  else if (value === false) {
+  else if (value == false) {
     doorLock("UNLOCK");
     lightSwitch("ON");
   }
@@ -118,72 +131,6 @@ function home(value) {
 
 
 
-var flagSuccess = false;
-var flagFail = false;
-function ajaxCall(URL_address, state1, state2, name) {
-  "use strict";
-  $.ajax({
-    url: URL_address,
-    async: true,
-    type: "GET",
-    dataType: "json",
-    data: {access_token: accessToken},
-    success: function (data) {
-      $('.status').css('display','none');
-      $('#connected').css('display','inline');
-      if (!flagSuccess) {
-        console.log('Connected to Photon!');
-        flagSuccess = true;
-      }
-      flagFail = false;
-      status = data.result;
-      if (status === true) { // locked - lightOn
-        locked();
-      } else if (status === false) { // unlocked - lightOff
-        unlocked();
-      }
-      console.log(name + ": " + status);
-    },
-    error: function () {
-      $('.status').css('display','none');
-      $('#disconnected').css('display','inline');
-      if (!flagFail) {
-        console.log('Disonnected from Photon!');
-        flagFail = true;
-      }
-      flagSuccess = false;
-    }
-  });
-}
-
-ajaxCall(requestURLLock, locked, unlocked, isLocked);
-ajaxCall(requestURLLight, lightOn, lightOff, isOn);
-
-/***** REFRESH status *****/
-function refresh() {
-  ajaxCall(requestURLLock, locked, unlocked, isLocked);
-  ajaxCall(requestURLLight, lightOn, lightOff, isOn);
-  // LOCK STATUS
-  $.getJSON(requestURLLock, function(json) {
-    if (json.result === true){ // locked
-      locked();
-    }
-    else if (json.result === false){ // unlocked
-      unlocked();
-    }
-  });
-  // LIGHTSTATUS
-  $.getJSON(requestURLLight, function(json) {
-    if (json.result === true){ // light on
-      lightOn();
-    }
-    else if (json.result === false){ // light off
-      lightOff();
-    }
-  });
-}
-
-
-
 // Continuosly running refresh function, every 10 ms
 setInterval(refresh, 10);
+
